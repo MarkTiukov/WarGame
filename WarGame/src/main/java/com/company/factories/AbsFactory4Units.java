3b21b20134fe0233db357babc9cdaae974ae5ec4
MarkTiukov <tyukov.mark@gmail.com>
2020-03-27 14:39:07 +0300

    First upload

---
+++ WarGame/src/main/java/com/company/factories/AbsFactory4Units.java
@@ -0,0 +1,8 @@
+package com.company.factories;
+
+import com.company.units.*;
+
+public interface AbsFactory4Units {
+    WarriorUnit createWarrior();
+    ArcherUnit createArcher();
+}
---
+++ WarGame/src/main/java/com/company/factories/AnimalFactory4Units.java
@@ -0,0 +1,18 @@
+package com.company.factories;
+
+import com.company.units.*;
+
+public class AnimalFactory4Units implements AbsFactory4Units {
+
+    @Override
+    public WarriorUnit createWarrior() {
+        WarriorUnit warrior = new WolfUnit();
+        return warrior;
+    }
+
+    @Override
+    public ArcherUnit createArcher() {
+        ArcherUnit archer = new MonkeyUnit();
+        return archer;
+    }
+}
