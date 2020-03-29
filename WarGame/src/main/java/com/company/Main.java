182aa34f3e10fb9ffb80df931b4ccb1b27a28b7c
MarkTiukov <tyukov.mark@gmail.com>
2020-03-29 15:57:58 +0300

    Fourth upload

--- WarGame/src/main/java/com/company/Main.java
+++ WarGame/src/main/java/com/company/Main.java
@@ -8,14 +8,33 @@ import com.company.fractions.Fraction;
 import com.company.fractions.FractionsId;
 import com.company.fractions.HumanFraction;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.concurrent.ConcurrentMap;
+
 public class Main {
 
     public static Fraction playableFractions[];
+    public static Scanner scanner = new Scanner(System.in);
+    public static ArrayList<String> availableFractons;
 
     public static void main(String[] args) {
-
-
-        ////
+        helloWords();
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals(Commands.TUTORIAL)) {
+                tutorial();
+                continue;
+            }
+            if (command.equals(Commands.FINISH)) {
+                break;
+            }
+            if (command.equals(Commands.START)) {
+                play();
+            }
+        }
+        /*
         playableFractions = new Fraction[2];
         playableFractions[0] = new HumanFraction();
         playableFractions[1] = new AnimalFraction();
@@ -26,5 +45,34 @@ public class Main {
         fieldBuilder.generateFractions();
         Field field = fieldBuilder.getField();
         field.drawInConsole();
+         */
     }
+
+    public static void helloWords() {
+        System.out.println("WELCOME TO WAR GAME");
+        System.out.println("ARE YOU READY??");
+        System.out.println("\n<You can read a tutorial by typing \"" + Commands.TUTORIAL + "\" without quotes>");
+        System.out.println("<To start type \"" + Commands.START + "\". To finish and exit type \"" + Commands.FINISH + "\">");
+    }
+
+    public static void tutorial() {
+        System.out.println("<SORRY, TUTORIAL IS NOT AVAILABLE YET>");
+    }
+
+    public static void play() {
+        availableFractons = new ArrayList<String>();
+        FractionsId.addAll(availableFractons);
+    }
+
+    public static void chooseNumber() {
+        System.out.println("<PLease, choose number of players: " + "2" + ">" );
+        playableFractions = new Fraction[scanner.nextInt()];
+    }
+
+    public static void chooseFractions() {
+        System.out.print("<Please, choose your fraction from available>");
+        System.out.println("<Available fractions:>");
+
+    }
+
 }
--- WarGame/src/main/java/com/company/fractions/FractionsId.java
+++ WarGame/src/main/java/com/company/fractions/FractionsId.java
@@ -1,8 +1,17 @@
 package com.company.fractions;
 
+import java.util.ArrayList;
+
 public class FractionsId {
+    private static int number = 2;
+
     public static final String PEOPLE_ID = "PEOPLE";
     public static final String ANIMALS_ID = "ANIMALS";
     // public static final String ROBOTS_ID = "ROBOTS";
+
+    public static void addAll(ArrayList<String> list) {
+        list.add(PEOPLE_ID);
+        list.add(ANIMALS_ID);
+    }
 
 }
