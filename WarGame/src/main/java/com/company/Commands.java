8750b9c66e5a3c475ec6ba5c58c98818739916bb
MarkTiukov <tyukov.mark@gmail.com>
2020-04-27 14:18:50 +0300

    Deleting DS_Store

--- WarGame/src/main/java/com/company/Commands.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Commands {
-    public static final String START = "start";
-    public static final String FINISH = "finish";
-    public static final String TUTORIAL = "tutorial";
-
-}
